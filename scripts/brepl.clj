8588762bd528270f97492af5e563fd0cf8284370
David Nolen <4312518+rwandezx9@users.noreply.github.com>
2017-08-08 09:38:53 -0400

    small tweaks

--- scripts/brepl.clj
+++ scripts/brepl.clj
@@ -4,14 +4,14 @@
   '[cljs.repl.browser :as browser])
 
 (b/build "src"
-         {:output-dir "out"
-          :modules {:moduleb {:output-to "out/moduleb.js"
-                              :entries #{'mies.moduleb}}
-                    :core {:output-to "out/core.js"
-                           :entries #{'mies.core}}}
-          :optimizations :none
-          :source-map false
-          :pretty-print true})
+  {:output-dir "out"
+   :modules {:moduleb {:output-to "out/moduleb.js"
+                       :entries #{'mies.moduleb}}
+             :core {:output-to "out/core.js"
+                    :entries #{'mies.core}}}
+   :optimizations :none
+   :verbose true                 
+   :source-map true})
 
 (repl/repl (browser/repl-env)
   :output-dir "out")
--- src/mies/core.cljs
+++ src/mies/core.cljs
@@ -1,11 +1,13 @@
 (ns mies.core
-  (:require [clojure.browser.repl :as repl]
-            [cljs.loader :as loader]
+  (:require [cljs.loader :as loader]
             [goog.dom :as gdom]))
 
 (.appendChild (gdom/getElement "test")
-              (gdom/createDom "a" #js {:onclick (fn [e]
-                                                  (js/console.log "Link to load Module B clicked")
-                                                  (loader/load :moduleb
-                                                               (fn [e] 
-                                                                 ((resolve 'mies.moduleb/init)))))} "Click to load Module B!"))
+  (gdom/createDom "a"
+    #js {:onclick
+         (fn [e]
+           (js/console.log "Link to load Module B clicked")
+           (loader/load :moduleb
+             (fn [e]
+               (.log js/console e)
+               ((resolve 'mies.moduleb/init)))))} "Click to load Module B!"))
